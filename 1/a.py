d21b4a608776f9b9589a0af4083f7b3ebd3b6275
dikos1705 <dikos1705@gmail.com>
2022-11-12 16:40:28 +0600

    First lesson

---
+++ 1/a.py
@@ -0,0 +1,15 @@
+# # print('Hello world')
+# print(15 /6) 
+# print(15 //6) 
+# print(3 **5)
+# print(15 % 6)
+# print(ord('A'))
+# print(chr(65))
+
+# print('Hello' + ' Dias')
+
+# print(1461, 1742,1568, sep ='!--!' )
+# print(1742)
+# print(1967)
+
+print((12**2 + 5 ** 2)** 0.5)
